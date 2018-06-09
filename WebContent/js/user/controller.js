a5bac529fa38176db21596cbb7911060fe4aa56d
AdithyanMundluru <C33780@NLLR4000294074.solon.prd>
2018-06-10 01:17:54 +0200

    updated

--- WebContent/js/user/controller.js
+++ WebContent/js/user/controller.js
@@ -4,26 +4,75 @@
 
 var myApp= angular.module("myModule",[]);
 
-var myController = function($scope,$http){ 
-  $scope.Money="2,231,324";
- $scope.donerSubmit=function(){
+var myController = function($scope,$http){
+	
+	$scope.Money = "";
+	$http({
+        method : "GET",
+        url : "http://23.101.72.31/rest/breakingshackles/v1/GetData/PoolBalance",
+        "async": true,
+		  "crossDomain": true,"headers": {
+			    "cache-control": "no-cache",
+			    "postman-token": "e2df787a-bfc1-dce8-dccb-4b88dcf03114",
+			    "Access-Control-Allow-Origin" : "*"
+			  }
+        
+    }).then(function mySuccess(response) {
+        $scope.Money = response.data;
+    }, function myError(response) {
+        $scope.Money = "";
+    });
+ 
+ $scope.DonarAccept=function(){
 	 	var object = {};
-		object.UserName = $scope.UserName
-	   object.Password= $scope.Password;
-	    object.Name=$scope.Name;
-	    object.NodeID=$scope.NodeId;
-	    object.Status=$scope.Status;
-	 $http.post('http://localhost:4468/BreakingShackles/api/api/function',object).then(function(response){
-		 $scope.message=response.data;
-	 },function(error){
-		 $scope.message="Service Fail";
-	 })
- } 
-	
+		$scope.donarName;
+		$scope.donarEmail;
+	    object.amount=$scope.Amount;
+	    object.contact=$scope.MobileNo;
+	    var url1="http://23.101.72.31:80/rest/breakingshackles/v1/AddToPool/AddToPool?Contact=%22+"+object.contact+"%22&Amount="+object.amount+","
+	    debugger;
+		$http({
+	        method : "GET",
+	        url : url1,
+	        "async": true,
+			  "crossDomain": true,"headers": {
+				    "cache-control": "no-cache",
+				    "postman-token": "e2df787a-bfc1-dce8-dccb-4b88dcf03114",
+				    "Access-Control-Allow-Origin" : "*"
+				  }
+	        
+	    }).then(function mySuccess(response) {
+	        $scope.Money = response.data;
+	    }, function myError(response) {
+	        $scope.Money = "";
+	    });
+ /*var settings = {
+		 "async": true,
+		 "crossDomain": true,
+		 "url": "http://23.101.72.31:80/rest/breakingshackles/v1/AddToPool/AddToPool?Contact=%22+31649018921%22&Amount="+$scope.Amount,
+		 "method": "GET",
+		 "headers": {
+		   "Cache-Control": "no-cache",
+		   "Postman-Token": "256fb08a-45ab-d654-5f5f-dd4b73e50a86"
+		 }
+		}
 
+		$.ajax(settings).done(function (response) {
+		 console.log(response);
+		});
+*/
+ $scope.NGO=function(){
+	 	var objectNgo = {};
+		objectNgo.NgoName=$scope.NgoName;
+		objectNgo.Email=$scope.Email;
 	
-		
-	
+	    objectNgo.NodeID=$scope.registrationKey;
+	    console.log(objectNgo)
+
+	    
 }
+	
+}		
+}	
 
 myApp.controller("myController",myController);
