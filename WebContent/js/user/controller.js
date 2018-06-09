a1f55668e84e09d3520b12f3dd0735490c48a4dc
AdithyanMundluru <C33780@NLLR4000294074.solon.prd>
2018-06-09 19:26:48 +0200

    updated

--- WebContent/js/user/controller.js
+++ WebContent/js/user/controller.js
@@ -1,12 +1,29 @@
 /**
  * 
  */
+
 var myApp= angular.module("myModule",[]);
 
-var myController = function($scope){ 
-	$scope.UserName = "";
-    $scope.Password = "";
-    $scope.Money="Eur 2837402"
+var myController = function($scope,$http){ 
+  $scope.Money="2,231,324";
+ $scope.donerSubmit=function(){
+	 	var object = {};
+		object.UserName = $scope.UserName
+	   object.Password= $scope.Password;
+	    object.Name=$scope.Name;
+	    object.NodeID=$scope.NodeId;
+	    object.Status=$scope.Status;
+	 $http.post('http://localhost:4468/BreakingShackles/api/api/function',object).then(function(response){
+		 $scope.message=response.data;
+	 },function(error){
+		 $scope.message="Service Fail";
+	 })
+ } 
+	
+
+	
+		
+	
 }
 
 myApp.controller("myController",myController);
