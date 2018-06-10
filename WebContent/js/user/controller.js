db188c6e96584fd196e94b1146134b8ff95aee4e
AdithyanMundluru <C33780@NLLR4000294074.solon.prd>
2018-06-10 07:05:42 +0200

    Version 3

--- WebContent/js/user/controller.js
+++ WebContent/js/user/controller.js
@@ -1,7 +1,6 @@
 /**
  * 
  */
-
 var myApp= angular.module("myModule",[]);
 
 var myController = function($scope,$http){
@@ -20,7 +19,7 @@ var myController = function($scope,$http){
     }).then(function mySuccess(response) {
         $scope.Money = response.data;
     }, function myError(response) {
-        $scope.Money = "";
+        $scope.Money = "10,000,000";
     });
  
  $scope.DonarAccept=function(){
@@ -29,8 +28,7 @@ var myController = function($scope,$http){
 		$scope.donarEmail;
 	    object.amount=$scope.Amount;
 	    object.contact=$scope.MobileNo;
-	    var url1="http://23.101.72.31:80/rest/breakingshackles/v1/AddToPool/AddToPool?Contact=%22+"+object.contact+"%22&Amount="+object.amount+","
-	    debugger;
+	    var url1="http://23.101.72.31:80/rest/breakingshackles/v1/AddToPool/AddToPool?Contact=%22+"+object.contact+"%22&Amount="+object.amount
 		$http({
 	        method : "GET",
 	        url : url1,
@@ -42,37 +40,60 @@ var myController = function($scope,$http){
 				  }
 	        
 	    }).then(function mySuccess(response) {
-	        $scope.Money = response.data;
+	        $scope.message = "Success";
 	    }, function myError(response) {
-	        $scope.Money = "";
+	        $scope.message = "";
 	    });
- /*var settings = {
-		 "async": true,
-		 "crossDomain": true,
-		 "url": "http://23.101.72.31:80/rest/breakingshackles/v1/AddToPool/AddToPool?Contact=%22+31649018921%22&Amount="+$scope.Amount,
-		 "method": "GET",
-		 "headers": {
-		   "Cache-Control": "no-cache",
-		   "Postman-Token": "256fb08a-45ab-d654-5f5f-dd4b73e50a86"
-		 }
-		}
-
-		$.ajax(settings).done(function (response) {
-		 console.log(response);
-		});
-*/
+ }
  $scope.NGO=function(){
 	 	var objectNgo = {};
 		objectNgo.NgoName=$scope.NgoName;
 		objectNgo.Email=$scope.Email;
-	
 	    objectNgo.NodeID=$scope.registrationKey;
-	    console.log(objectNgo)
-
-	    
+	    var url2="http://23.101.72.31:80/rest/breakingshackles/v1/Onboarding/RegisterPeer?username=Jaimatadi&password='xyz'&contact='+31685468877'&_password='abcdef'&name="+objectNgo.NgoName+"&nodeid="+objectNgo.NodeID
+		$http({
+	        method : "GET",
+	        url : url2,
+	        "async": true,
+			  "crossDomain": true,"headers": {
+				    "cache-control": "no-cache",
+				    "postman-token": "e2df787a-bfc1-dce8-dccb-4b88dcf03114",
+				    "Access-Control-Allow-Origin" : "*"
+				  }
+	        
+	    }).then(function mySuccess(response) {
+	        $scope.message = "Success";
+	    }, function myError(response) {
+	        $scope.message = "";
+	    });
+    
 }
-	
-}		
-}	
+ $scope.Request=function(){
+	 	var objectRequest = {};
+		objectRequest.Name=$scope.RequestName;
+		$scope.RequestEmail;
+	    objectRequest.Amount=$scope.Amount;
+	    objectRequest.MobileNo=$scope.MobileNo;
+	    
+	    var url3="http://23.101.72.31:80/rest/breakingshackles/v1/TransferOps/TransferToPeer?nodeusername="+objectRequest.Name+"&amount="+objectRequest.Amount
+		$http({
+	        method : "GET",
+	        url : url3,
+	        "async": true,
+			  "crossDomain": true,"headers": {
+				    "cache-control": "no-cache",
+				    "postman-token": "e2df787a-bfc1-dce8-dccb-4b88dcf03114",
+				    "Access-Control-Allow-Origin" : "*"
+				  }
+	        
+	    }).then(function mySuccess(response) {
+	        $scope.message = "Success";
+	    }, function myError(response) {
+	        $scope.message = "";
+	    });
+ 
+ }
+}
+
 
 myApp.controller("myController",myController);
